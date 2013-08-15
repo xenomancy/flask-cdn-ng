c11ea630ed163e85c39d3efb48b15cb603bc73b3
William Fagan <will@wichitacode.com>
2013-08-15 16:44:55 -0500

    Unify string quotes.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'Flask'
     ],
-    test_suite="tests",
+    test_suite='tests',
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
